bb0e064009fd3f3baee438601f24a282bbc2952d
Neslihan Minaz <minaz_nesli@hotmail.com>
2021-07-15 13:13:25 +0200

    home and about me

--- myBlog/src/app/app.module.ts
+++ myBlog/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
